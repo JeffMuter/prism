958d17377a99ea7f0391229c9634120f03689da4
JeffMuter <muterjeffery@gmail.com>
2024-06-21 22:30:26 -0400

    made a map!

--- main/main.go
+++ main/main.go
@@ -13,12 +13,32 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("Width: %d, Height: %d\n", width, height)
-	// for i := 0; i < width; i++ {
-	// 	// one line of ascii
-	// 	fmt.Println("***")
-	// 	for j := 0; j < height; j++ {
-	// 		// one height?
-	// 	}
 
-	// }
+	// top line, 50 char long
+	for i := 0; i < 50; i++ {
+		fmt.Print("*")
+	}
+
+	//make new line
+	// 1 * , 48 spaces, 1 * , repeat for 20 lines
+	for i := 0; i < 18; i++ {
+		fmt.Println()
+		fmt.Print("*")
+
+		for j := 0; j < 48; j++ {
+			if j == 23 && i == 9 {
+				fmt.Print("@")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+
+		fmt.Print("*")
+	}
+	fmt.Println()
+
+	// bottom line, 50 char long
+	for i := 0; i < 50; i++ {
+		fmt.Print("*")
+	}
 }
