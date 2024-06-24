56d68adcd098bfe3a3b9831af41c2f34e1f4f72a
JeffMuter <muterjeffery@gmail.com>
2024-06-23 23:29:39 -0400

    refactoring the render functions, getting shit out of the main func, and determining how we want to handle many objects on the screen. in progress.

--- main/main.go
+++ main/main.go
@@ -6,74 +6,23 @@ import (
 	"prism/render"
 )
 
-type art struct {
-	width  int
-	height int
-	art    []string
-}
-
-var house art = art{
-	width:  70,
-	height: 14,
-	art: []string{
-		"    .                  .-.    .  _   *     _   .",
-		"           *          /   \\     ((       _/ \\       *    .",
-		"         _    .   .--'/\\/_ \\     `      /    \\  *    ___",
-		"     *  / \\_    _/ ^      \\/\\\\__        /\\/\\  /\\  __/   \\ *",
-		"       /    \\  /    .'   _/  /  \\  *' /    \\/  \\/ .'\\_/\\   .",
-		"  .   /\\/\\  /\\/ :' __  ^/  ^/    `--./.'  ^  `-.\\_    _:\\ _",
-		"     /    \\/  \\  _/  \\-' __/.' ^ _   \\_   .\\'   _/ \\ .  __/ \\",
-		"   /\\  .-   `. \\/     \\ / -.   _/ \\ -. `_/   \\ /    `._/  ^  \\",
-		"  /  `-.__ ^   / .-'.--'    . /    `--./ .-'  `-.  `-. `.  -  `.",
-		"@/        `.  / /      `-.   /  .-'   / .   .'   \\    \\  \\  .-  \\%",
-		"@&8jgs@@%% @)&@&(88&@.-_=_-=_-=_-=_-=_.8@% &@&&8(8%@%8)(8@%8 8%@)%",
-		"@88:::&(&8&&8:::::%&`.~-_~~-~~_~-~_~-~~=.'@(&%::::%@8&8)::&#@8::::",
-		"::::::8%@@%:::::@%&8:`.=~~-.~~-.~~=..~'8::::::::&@8:::::&8:::::",
-		"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::.",
-	},
-}
-
 func main() {
 
-	termWidth := 100
-	termHeight := 20
+	// get user location
 
-	var userLat float32 = 32.234
-	var userLong float32 = -48.254
+	// get objects that will be on screen
+	// database.getArtForDisplay()
 
-	var objLat float32 = 32.232
-	var objLong float32 = -48.235
+	// get screen size
+	// term.GetTermSize
 
-	// termWidth, termHeight, err := operating_system.GetTerminalSize()
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-	// 	os.Exit(1)
-	// }
+	// render the screen to display to the user
+	// render.RenderScreen()
 
-	fmt.Printf("Width: %d, Height: %d\n", termWidth, termHeight)
-
-	objCoordinates, err := findObjectCoordinate(userLong, userLat, objLong, objLat, termWidth, termHeight)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Printf("Width: %d, Height: %d\n", termWidth, termHeight)
-
-	canvas := make([][]rune, termHeight)
-	for i := range canvas {
-		canvas[i] = make([]rune, termWidth)
-		for j := range canvas[i] {
-			canvas[i][j] = '='
-		}
-	}
-
-	if userLat-objLat > -1 && userLat-objLat < 1 && userLong-objLong > -1 && userLong-objLong < 1 {
-		addObjectToCanvas(canvas, house, objCoordinates[0], objCoordinates[1])
-	}
+	canvas := render.RenderScreen()
 
 	for _, line := range canvas {
 		fmt.Println(string(line))
 	}
 
-	render.Render()
 }
--- render/render.go
+++ render/render.go
@@ -1,70 +1,164 @@
 package render
 
 import (
-	"errors"
 	"fmt"
 )
 
-func Render() {
-
+type object struct {
+	id          int
+	user_id     int
+	object_id   int
+	name        string
+	latitude    float32
+	longitude   float32
+	object_type string
+	description string
+	art         art
+	xCoordinate int
+	yCoordinate int
 }
 
-func findObjectCoordinate(userLong, userLat, objLong, objLat float32, scrWidth, scrHeight int) ([2]int, error) {
+type art struct {
+	width  int
+	height int
+	art    []string
+}
 
-	var objPosition = [2]int{0, 0}
-	latDistance := objLat - userLat
-	longDistance := objLong - userLong
+var mountain art = art{
+	width:  72,
+	height: 14,
+	art: []string{
+		"    .                  .-.    .  _   *     _   .                        ",
+		"           *          /   \\     ((       _/ \\       *    .            ",
+		"         _    .   .--'/\\/_ \\     `      /    \\  *    ___             ",
+		"     *  / \\_    _/ ^      \\/\\\\__        /\\/\\  /\\  __/   \\ *     ",
+		"       /    \\  /    .'   _/  /  \\  *' /    \\/  \\/ .'\\_/\\   .      ",
+		"  .   /\\/\\  /\\/ :' __  ^/  ^/    `--./.'  ^  `-.\\_    _:\\ _        ",
+		"     /    \\/  \\  _/  \\-' __/.' ^ _   \\_   .\\'   _/ \\ .  __/ \\    ",
+		"   /\\  .-   `. \\/     \\ / -.   _/ \\ -. `_/   \\ /    `._/  ^  \\    ",
+		"  /  `-.__ ^   / .-'.--'    . /    `--./ .-'  `-.  `-. `.  -  `.        ",
+		"@/        `.  / /      `-.   /  .-'   / .   .'   \\    \\  \\  .-  \\%  ",
+		"@&8jgs@@%% @)&@&(88&@.-_=_-=_-=_-=_-=_.8@% &@&&8(8%@%8)(8@%8 8%@)%      ",
+		"@88:::&(&8&&8:::::%&`.~-_~~-~~_~-~_~-~~=.'@(&%::::%@8&8)::&#@8::::::    ",
+		"::::::8%@@%:::::@%&8:`.=~~-.~~-.~~=..~'8::::::::&@8:::::&8::::::::::::  ",
+		"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::",
+	},
+}
 
-	if latDistance > 1 || latDistance < -1 || longDistance > 1 || longDistance < -1 {
-		// if true, will NOT be visible on screen
-		return objPosition, errors.New(" object too far to project to map")
+func RenderScreen() [][]rune {
+	// get terminal size
+	// real way to get user screen
+
+	// termWidth, termHeight, err := operating_system.GetTerminalSize()
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	// 	os.Exit(1)
+	// }
+
+	// dummy terminal data
+	termWidth := 100
+	termHeight := 20
+	fmt.Printf("Width: %d, Height: %d\n", termWidth, termHeight)
+
+	// get user location
+	// dummy user data
+	var userLat float32 = 12.54
+	var userLong float32 = -144.54
+
+	// get obj locations
+	// dummy obj data
+	objLandmark := object{0, 0, 0, "node", 12.01, -143.32, "node", "first node", art{5, 2, []string{"12345", "12345"}}}
+	objWorker := object{0, 0, 0, "node", 13.00, -143.82, "worker", "first worker", art{1, 1, []string{"@"}}}
+	var objectsToRender []object = []object{objLandmark, objWorker}
+
+	// get each obj coordinates
+
+	objCoordinates, err :=
+		findObjectCoordinate(userLong, userLat, objectsToRender, termWidth, termHeight)
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	// gets the range between 0-2 for the equation below
-	latDistance += 1
-	longDistance += 1
+	canvas := make([][]rune, termHeight)
+	for i := range canvas {
+		canvas[i] = make([]rune, termWidth)
+		for j := range canvas[i] {
+			canvas[i][j] = '='
+		}
+	}
 
-	var horizontalDistancePerChar float32 = float32(2) / float32(scrWidth)
-	var verticalDistancePerChar float32 = float32(2) / float32(scrHeight)
+	addObjectsToCanvas(canvas, objectsToRender, objCoordinates)
 
-	// Calculate the position in screen coordinates
-	objPosition[0] = int(latDistance / horizontalDistancePerChar)
-	objPosition[1] = scrHeight - int(longDistance/verticalDistancePerChar)
-
-	fmt.Println(objPosition)
-	return objPosition, nil
+	return canvas
 }
 
-func addObjectToCanvas(canvas [][]rune, art art, posX int, posY int) {
+// get user location
+// get all obj locations
+// validate obj locations as close enough to user
+// calc obj coordinates on screen
+
+func findObjectCoordinate(userLong, userLat float32, objects []object, scrWidth, scrHeight int) ([]objects, error) {
+
+	var objCoordinates [][2]int
+
+	for i, object := range objects {
+		objLat := object.latitude
+		objLong := object.longitude
+
+		latDistance := objLat - userLat
+		longDistance := objLong - userLong
+
+		// gets the range between 0-2 for the equation below
+		latDistance += 1
+		longDistance += 1
+
+		var horizontalDistancePerChar float32 = float32(2) / float32(scrWidth)
+		var verticalDistancePerChar float32 = float32(2) / float32(scrHeight)
+
+		// Calculate the position in screen coordinates, place into return slice
+
+		if userLat-object.latitude > -1 && userLat-object.latitude < 1 && userLong-object.longitude > -1 && userLong-object.longitude < 1 {
+			objects[i].xCoordinate = int(latDistance / horizontalDistancePerChar)
+			objects[i].yCoordinate = scrHeight - int(longDistance/verticalDistancePerChar)
+		}
+
+		fmt.Printf("\nObject: %s, [%d, %d]", object.name, objCoordinates[i][0], objCoordinates[i][1])
+	}
+	// TODO: fiter objects so that the object with the lowest vertical coordinates always on top...
+
+	return objects, nil
+}
+
+func addObjectsToCanvas(canvas [][]rune, objects []object, objectsCoordinates [][2]int) {
 	canvasHeight := len(canvas)
 	canvasWidth := len(canvas[0])
 
-	fmt.Printf("Canvas Size: Width: %d, Height: %d\n", canvasWidth, canvasHeight)
-	fmt.Printf("Art Position: X: %d, Y: %d\n", posX, posY)
-
-	for y := 0; y < art.height; y++ {
-		artY := posY + y
-		if artY < 0 || artY >= canvasHeight {
-			fmt.Printf("Skipping line %d: artY (%d) out of canvas bounds\n", y, artY)
-			continue
-		}
-
-		for x := 0; x < art.width; x++ {
-			artX := posX + x
-			if artX < 0 || artX >= canvasWidth {
-				fmt.Printf("Skipping column %d: artX (%d) out of canvas bounds\n", x, artX)
+	// fmt.Printf("Canvas Size: Width: %d, Height: %d\n", canvasWidth, canvasHeight)
+	// fmt.Printf("Art Position: X: %d, Y: %d\n", posX, posY)
+	for _, object := range objects {
+		for y := 0; y < object.art.height; y++ {
+			artY := objectCoordinates[] posY + y
+			if artY < 0 || artY >= canvasHeight {
+				fmt.Printf("Skipping line %d: artY (%d) out of canvas bounds\n", y, artY)
 				continue
 			}
 
-			// Ensure the art slice indices are also within bounds
-			if y >= len(art.art) || x >= len(art.art[y]) {
-				fmt.Printf("Invalid art index [%d][%d] accessed, skipping draw.\n", y, x)
-				continue
-			}
+			for x := 0; x < art.width; x++ {
+				artX := posX + x
+				if artX < 0 || artX >= canvasWidth {
+					fmt.Printf("Skipping column %d: artX (%d) out of canvas bounds\n", x, artX)
+					continue
+				}
 
-			fmt.Printf("Drawing at canvas[%d][%d]\n", artY, artX)
-			canvas[artY][artX] = rune(art.art[y][x])
+				// Ensure the art slice indices are also within bounds
+				if y >= len(art.art) || x >= len(art.art[y]) {
+					fmt.Printf("Invalid art index [%d][%d] accessed, skipping draw.\n", y, x)
+					continue
+				}
+
+				fmt.Printf("Drawing at canvas[%d][%d]\n", artY, artX)
+				canvas[artY][artX] = rune(art.art[y][x])
+			}
 		}
 	}
-
 }
