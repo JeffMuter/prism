2fea7dd360342fcd11c347cc63db86238e40ce63
JeffMuter <muterjeffery@gmail.com>
2024-06-24 01:19:19 -0400

    bubble sort. needs testing. render is funtional

--- render/render.go
+++ render/render.go
@@ -63,17 +63,21 @@ func RenderScreen() [][]rune {
 	// get user location
 	// dummy user data
 	var userLat float32 = 12.54
-	var userLong float32 = -144.54
+	var userLong float32 = -143.54
 
 	// get obj locations
 	// dummy obj data
-	objLandmark := object{0, 0, 0, "node", 12.01, -143.32, "node", "first node", art{5, 2, []string{"12345", "12345"}}}
-	objWorker := object{0, 0, 0, "node", 13.00, -143.82, "worker", "first worker", art{1, 1, []string{"@"}}}
-	var objectsToRender []object = []object{objLandmark, objWorker}
+	objLandmark := object{0, 0, 0, "node1", 12.01, -143.32, "node", "first node", art{5, 2, []string{"12345", "12345"}}, 0, 0}
+	objWorker := object{0, 0, 0, "node2", 11.80, -143.02, "worker", "first worker", mountain, 0, 0}
+	obj3 := object{0, 0, 0, "node3", 12.01, -143.32, "node", "first node", art{5, 2, []string{"12345", "12345"}}, 0, 0}
+	obj4 := object{0, 0, 0, "node4", 11.80, -143.02, "worker", "first worker", mountain, 0, 0}
+	obj5 := object{0, 0, 0, "node5", 11.67, -143.32, "node", "first node", art{5, 2, []string{"12345", "12345"}}, 0, 0}
+	obj6 := object{0, 0, 0, "node6", 13.120, -143.02, "worker", "first worker", mountain, 0, 0}
+	var objectsToRender []object = []object{objLandmark, objWorker, obj3, obj4, obj5, obj6}
 
 	// get each obj coordinates
 
-	objCoordinates, err :=
+	objectsToRender, err :=
 		findObjectCoordinate(userLong, userLat, objectsToRender, termWidth, termHeight)
 	if err != nil {
 		fmt.Println(err)
@@ -87,7 +91,7 @@ func RenderScreen() [][]rune {
 		}
 	}
 
-	addObjectsToCanvas(canvas, objectsToRender, objCoordinates)
+	addObjectsToCanvas(canvas, objectsToRender)
 
 	return canvas
 }
@@ -97,9 +101,8 @@ func RenderScreen() [][]rune {
 // validate obj locations as close enough to user
 // calc obj coordinates on screen
 
-func findObjectCoordinate(userLong, userLat float32, objects []object, scrWidth, scrHeight int) ([]objects, error) {
-
-	var objCoordinates [][2]int
+func findObjectCoordinate(userLong, userLat float32, objects []object, scrWidth, scrHeight int) ([]object, error) {
+	var validatedObjects []object
 
 	for i, object := range objects {
 		objLat := object.latitude
@@ -120,45 +123,60 @@ func findObjectCoordinate(userLong, userLat float32, objects []object, scrWidth,
 		if userLat-object.latitude > -1 && userLat-object.latitude < 1 && userLong-object.longitude > -1 && userLong-object.longitude < 1 {
 			objects[i].xCoordinate = int(latDistance / horizontalDistancePerChar)
 			objects[i].yCoordinate = scrHeight - int(longDistance/verticalDistancePerChar)
+			validatedObjects = append(validatedObjects, objects[i])
 		}
 
-		fmt.Printf("\nObject: %s, [%d, %d]", object.name, objCoordinates[i][0], objCoordinates[i][1])
+		//	fmt.Printf("\nObject: %s, [%d, %d]", object.name, objCoordinates[i][0], objCoordinates[i][1])
 	}
 	// TODO: fiter objects so that the object with the lowest vertical coordinates always on top...
+	validatedObjects = orderObjectSlice(validatedObjects)
 
-	return objects, nil
+	return validatedObjects, nil
 }
 
-func addObjectsToCanvas(canvas [][]rune, objects []object, objectsCoordinates [][2]int) {
+func addObjectsToCanvas(canvas [][]rune, objects []object) {
 	canvasHeight := len(canvas)
 	canvasWidth := len(canvas[0])
 
-	// fmt.Printf("Canvas Size: Width: %d, Height: %d\n", canvasWidth, canvasHeight)
-	// fmt.Printf("Art Position: X: %d, Y: %d\n", posX, posY)
+	// on first glance, I think something smells fishy here. not sure y < object.art.height should be the bounds for y...
 	for _, object := range objects {
 		for y := 0; y < object.art.height; y++ {
-			artY := objectCoordinates[] posY + y
+			artY := object.yCoordinate + y
 			if artY < 0 || artY >= canvasHeight {
 				fmt.Printf("Skipping line %d: artY (%d) out of canvas bounds\n", y, artY)
 				continue
 			}
 
-			for x := 0; x < art.width; x++ {
-				artX := posX + x
+			for x := 0; x < object.art.width; x++ {
+				artX := object.xCoordinate + x
 				if artX < 0 || artX >= canvasWidth {
 					fmt.Printf("Skipping column %d: artX (%d) out of canvas bounds\n", x, artX)
 					continue
 				}
 
 				// Ensure the art slice indices are also within bounds
-				if y >= len(art.art) || x >= len(art.art[y]) {
+				if y >= len(object.art.art) || x >= len(object.art.art[y]) {
 					fmt.Printf("Invalid art index [%d][%d] accessed, skipping draw.\n", y, x)
 					continue
 				}
 
 				fmt.Printf("Drawing at canvas[%d][%d]\n", artY, artX)
-				canvas[artY][artX] = rune(art.art[y][x])
+				canvas[artY][artX] = rune(object.art.art[y][x])
 			}
 		}
 	}
+}
+
+func orderObjectSlice(objects []object) []object {
+	// bubble sort
+	for j := len(objects); j > 0; j-- {
+		for i := 0; i < len(objects)-1; i++ {
+			if objects[i].yCoordinate+objects[i].art.height < objects[i+1].yCoordinate+objects[i+1].art.height {
+				tempObj := objects[i]
+				objects[i] = objects[i+1]
+				objects[i+1] = tempObj
+			}
+		}
+	}
+	return objects
 }
