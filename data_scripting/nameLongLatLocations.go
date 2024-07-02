79a200403861d18f673aa05d6a48ac1a11221493
JeffMuter <muterjeffery@gmail.com>
2024-07-02 16:32:04 -0400

    added a few thousand rows to the db

--- data_scripting/nameLongLatLocations.go
+++ data_scripting/nameLongLatLocations.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	_ "github.com/lib/pq"
+	"log"
 	"os"
 	"prism/database"
+	"strconv"
 	"strings"
 )
 
@@ -22,10 +24,30 @@ func AddLocationsToDb() {
 	}
 
 	scanner := bufio.NewScanner(file)
+
 	for scanner.Scan() {
 		nameLocationSlice := strings.Split(scanner.Text(), "	")
-		for output := range nameLocationSlice {
-			fmt.Println(output)
+		var loctype string = "city"
+		var description = "a large city in the distance... probably"
+		var art string = "city_art"
+
+		var name string = nameLocationSlice[0]
+		var lat float64
+		var long float64
+		lat, err = strconv.ParseFloat(nameLocationSlice[1], 64)
+		if err != nil {
+			fmt.Println("error converting lat to float:", err)
 		}
+		long, err = strconv.ParseFloat(nameLocationSlice[2], 64)
+		if err != nil {
+			fmt.Println("error converting lat to float:", err)
+		}
+
+		query := `INSERT INTO locations (location_type, longitude, latitude, name, description, art) VALUES ($1, $2, $3, $4, $5, $6 )`
+		_, err := db.Exec(query, loctype, long, lat, name, description, art)
+		if err != nil {
+			log.Panic("executing to db...", err)
+		}
+		fmt.Printf("name: %s, lat: %v, long: %v\n", name, lat, long)
 	}
 }
--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
 	"prism/data_scripting"
-	"prism/nodes"
-	"prism/render"
-	"prism/user"
-	"strings"
 )
 
 func main() {
@@ -20,34 +13,34 @@ func main() {
 	//}
 	data_scripting.AddLocationsToDb()
 
-	canvas := render.PaintScreen()
-
-	for _, line := range canvas {
-		fmt.Println(string(line))
-	}
-
-	reader := bufio.NewReader(os.Stdin)
-
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Invalid input: ", err)
-	}
-	input = strings.TrimSpace(input)
-
-	if input == "ping" {
-		lat, long, err := user.Ping()
-		if err != nil {
-			fmt.Println("ping err: ", err)
-		}
-		fmt.Printf("lat: %v, long: %v.\n", lat, long)
-	} else if input == "node" {
-		userLat, userLong, err := user.Ping()
-		if err != nil {
-			fmt.Println(err)
-		}
-		err = nodes.CreateNode(1, userLat, userLong)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
+	//canvas := render.PaintScreen()
+	//
+	//for _, line := range canvas {
+	//	fmt.Println(string(line))
+	//}
+	//
+	//reader := bufio.NewReader(os.Stdin)
+	//
+	//input, err := reader.ReadString('\n')
+	//if err != nil {
+	//	fmt.Println("Invalid input: ", err)
+	//}
+	//input = strings.TrimSpace(input)
+	//
+	//if input == "ping" {
+	//	lat, long, err := user.Ping()
+	//	if err != nil {
+	//		fmt.Println("ping err: ", err)
+	//	}
+	//	fmt.Printf("lat: %v, long: %v.\n", lat, long)
+	//} else if input == "node" {
+	//	userLat, userLong, err := user.Ping()
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//	err = nodes.CreateNode(1, userLat, userLong)
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//}
 }
