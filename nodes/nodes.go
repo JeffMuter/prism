997659640d11e40396da7d44ea72f93eb88773dc
JeffMuter <muterjeffery@gmail.com>
2024-07-08 08:38:14 -0400

    success on add node functionality. Can make a node, added to correct tables, and can't make it too close to another location.

--- nodes/nodes.go
+++ nodes/nodes.go
@@ -27,6 +27,7 @@ func CreateNode(user user.User) error {
 	db := database.OpenDatabase()
 	defer db.Close()
 	var locations []Location
+
 	//this num signifies 10 miles in lat/long degrees. We're using this to
 	// determine the max / min lat&long to determine if the node we want to place is too close to another node.
 	var latLongRange float64 = 0.145
@@ -35,7 +36,7 @@ func CreateNode(user user.User) error {
 	minLat, maxLat, minLong, maxLong := util.GetMaxLocationRanges(latLongRange, user.Latitude, user.Longitude)
 
 	query :=
-		"SELECT longitude, latitude FROM locations LEFT JOIN user_locations ON locations.id = user_locations.location_id WHERE user_locations.user_id = $1 OR locations.default_accessible = TRUE"
+		"SELECT name, longitude, latitude FROM locations LEFT JOIN user_locations ON locations.id = user_locations.location_id WHERE user_locations.user_id = $1 OR locations.default_accessible = TRUE"
 
 	rows, err := db.Query(query, user.Id)
 	if err != nil {
@@ -44,26 +45,25 @@ func CreateNode(user user.User) error {
 
 	for rows.Next() {
 		var location Location
-		err := rows.Scan(&location.Longitude, &location.Latitude)
+		err := rows.Scan(&location.Name, &location.Latitude, &location.Longitude)
 		if err != nil {
 			log.Fatal(err)
 		}
 		locations = append(locations, location)
 	}
 	for _, loc := range locations {
-		fmt.Printf("ID: %d, Type: %s, Name: %s, Description: %s\n", loc.Id, loc.LocationType, loc.Name, loc.Description)
-		if loc.Latitude < minLat || loc.Latitude > maxLat || loc.Longitude < minLong || loc.Longitude > maxLong {
-			return fmt.Errorf("node location too close to another")
+		if loc.Latitude > minLat && loc.Latitude < maxLat && loc.Longitude > minLong && loc.Longitude < maxLong {
+			return fmt.Errorf("node location too close to another: %s", loc.Name)
 		}
 	}
 
 	// add node to locations
-	query = "INSERT INTO locations (default_accessible, location_type, longitude, latitude, name, description, art) VALUES ($1, $2, $3, $4, $5, $6, $7)"
-	retRow := db.QueryRow(query, "false", "node", user.Latitude, user.Longitude, "new node", "new node description", "node")
-	//not totally sure how to properly err handle for retRow...
-	fmt.Println(retRow.Scan())
+	query = "INSERT INTO locations (default_accessible, location_type, longitude, latitude, name, description, art) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id"
+	var newLocationRowId int
+	db.QueryRow(query, "false", "node", user.Latitude, user.Longitude, "new node", "new node description", "node").Scan(&newLocationRowId)
+
 	query = "INSERT INTO user_locations (user_id, location_id) VALUES ($1, $2)"
-	retRow = db.QueryRow(query)
+	db.QueryRow(query, user.Id, newLocationRowId)
 
 	return nil
 }
