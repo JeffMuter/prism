a9bdb1b5439dde5ef90bf1fc07661202cf153b2e
JeffMuter <muterjeffery@gmail.com>
2025-07-24 20:48:39 +0000

    completed sqlc refactor for locations.go to the best of my ability without testing being able to be done. but progress, and learned the structure of how to implement said fixes.

--- locations/locations.go
+++ locations/locations.go
@@ -11,9 +11,9 @@ import (
 )
 
 // create location adds location to the location, user_location, and adds an egg to user's inventory.
+// idk if egg is being created here bro
 func CreateLocation(user user.User, locName string, locTypeId int) (int, error) {
 	var newLocationRowId int
-	var newUsersLocsId int
 
 	//this num signifies 10 miles in lat/long degrees. We're using this to
 	// determine the max / min lat&long to determine if the node we want to place is too close to another node.
@@ -35,20 +35,6 @@ func CreateLocation(user user.User, locName string, locTypeId int) (int, error) 
 	ctx := context.Background()
 
 	// add node to locations
-	query := `INSERT INTO locations 
-		(
-		default_accessible, 
-		latitude, 
-		longitude, 
-		name, 
-		description, 
-		art, 
-		location_type_id, 
-		is_user_created
-		) 
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) 
-		RETURNING id`
-
 	params := sqlc.CreateLocationParams{
 		DefaultAccessible: false,
 		Latitude:          user.Latitude,
@@ -60,21 +46,20 @@ func CreateLocation(user user.User, locName string, locTypeId int) (int, error) 
 		IsUserCreated:     true,
 	}
 
-	_, err = queries.CreateLocation(ctx, params)
+	locId, err := queries.CreateLocation(ctx, params)
 
-	// TODO: cannot be using all these lame hard coded values here...
-	// made the location art custom to the type of location...
+	newParams := sqlc.CreateUserLocationParams{
+		UserID:     int64(user.Id),
+		LocationID: locId,
+		Name:       db.StringToNullString(locName),
+	}
 
-	db.QueryRow(query, "false", user.Latitude, user.Longitude, locName, "new node description", "node", locTypeId, true).Scan(&newLocationRowId)
-
-	query = `INSERT INTO users_locations 
-	(user_id, location_id, name) 
-	VALUES ($1, $2, $3) RETURNING id`
-	err = db.QueryRow(query, user.Id, newLocationRowId, locName).Scan(&newUsersLocsId)
+	// not sure if we're getting the right int here.
+	newLocId, err := queries.CreateUserLocation(ctx, newParams)
 	if err != nil {
 		return newLocationRowId, fmt.Errorf("error inserting users_locations when creating new location: %v\n", err)
 	}
-	return newUsersLocsId, nil
+	return int(newLocId), nil
 }
 
 // GetAllLocations used to get locations from the database, placed into a location type.
@@ -97,47 +82,38 @@ func GetAllLocations(user user.User) ([]sqlc.GetVisibleLocationsByUserRow, error
 // range, and if no other locations are inside it, creates the new node. Returns the id of the newly connected location.
 func ConnectToLocation(user user.User) (int, error) {
 
-	var newUsersLocsId int
-
 	queries := db.GetQueries()
 	ctx := context.Background()
 
-	// get all locations currently not associated to this user
-	query := `SELECT 
-		l.id, 
-		ul.name, 
-		l.latitude, 
-		l.longitude 
-	FROM locations l 
-	LEFT JOIN users_locations ul ON l.id = ul.location_id 
-	AND ul.user_id = $1 
-	WHERE ul.user_id IS NULL`
-	rows, err := db.Query(query, user.Id)
+	var newUsersLocsId int
+
+	// get all locations this user has not connected to
+	locs, err := queries.GetUnconnectedLocations(ctx, int64(user.Id))
 	if err != nil {
 		return newUsersLocsId, fmt.Errorf("err querying db for connect to node: %v", err)
 	}
-	// a range of roughly .1 miles in lat/long.
+
+	// a range of roughly .1 miles in lat/long, used to determine if user should be allowed to connect to this loc
 	minLat, maxLat, minLong, maxLong := util.GetMaxLocationRanges(.5, user.Latitude, user.Longitude)
-	var locations []Location
 
-	for rows.Next() {
-		var location Location
-		err := rows.Scan(&location.Id, &location.Name, &location.Latitude, &location.Longitude)
-		if err != nil {
-			return newUsersLocsId, fmt.Errorf("error scanning values from row in rows.next, err: %v\n", err)
-		}
-		if location.Latitude < maxLat && location.Latitude > minLat && location.Longitude < maxLong && location.Longitude > minLong {
+	for _, loc := range locs {
 
-			query = `INSERT INTO users_locations (user_id, location_id) 
-			VALUES ($1, $2) 
-			RETURNING id;`
-			err = db.QueryRow(query, user.Id, location.Id).Scan(&newUsersLocsId)
-			if err != nil {
-				return newUsersLocsId, fmt.Errorf("error inserting new users_locations while connecting to new location: %v\n", err)
+		// if this location is within range of the user, they can connect
+		if loc.Latitude < maxLat && loc.Latitude > minLat && loc.Longitude < maxLong && loc.Longitude > minLong {
+
+			params := sqlc.CreateUserLocationParams{
+				UserID:     int64(user.Id),
+				LocationID: int64(loc.ID),
+				Name:       loc.Name,
 			}
-			return newUsersLocsId, nil
+
+			// create a user-location
+			userLocId, err := queries.CreateUserLocation(ctx, params)
+			if err != nil {
+				return int(userLocId), fmt.Errorf("error inserting new users_locations while connecting to new location: %v\n", err)
+			}
+			return int(userLocId), nil
 		}
-		locations = append(locations, location)
 	}
 
 	return newUsersLocsId, errors.New("could not find a node close enough to connect to")
@@ -166,11 +142,7 @@ func RemoveWorkerFromNode(locationId int) error {
 	queries := db.GetQueries()
 	ctx := context.Background()
 
-	query := `UPDATE users_locations 
-	SET worker_count = worker_count - 1 
-	WHERE users_locations.id = $1`
-
-	_, err := db.Exec(query, locationId)
+	err := queries.DecrementWorkerCount(ctx, int64(locationId))
 	if err != nil {
 		fmt.Println("Error subtracting from worker_count", err)
 		return fmt.Errorf("error subtracting 1 from worker_count on users_locations table: %w,", err)
@@ -185,11 +157,7 @@ func AddWorkerToNode(locationId int) error {
 	queries := db.GetQueries()
 	ctx := context.Background()
 
-	query := `UPDATE users_locations 
-	SET worker_count = worker_count + 1 
-	WHERE users_locations.id = $1`
-
-	_, err := db.Exec(query, locationId)
+	err := queries.IncrementWorkerCount(ctx, int64(locationId))
 	if err != nil {
 		fmt.Println("Error subtracting from worker_count", err)
 		return fmt.Errorf("error adding to worker_count on users_locations table: %w,", err)
@@ -206,54 +174,24 @@ func GetTaskNamesForLocationType(locationTypeId int) ([]string, error) {
 	queries := db.GetQueries()
 	ctx := context.Background()
 
-	var taskTypes []string
-	query := `SELECT tt.name 
-	FROM task_types tt 
-	JOIN location_types_tasks ltt ON tt.id = ltt.task_type_id 
-	JOIN location_types lt ON ltt.location_type_id = lt.id 
-	WHERE lt.id = $1`
-
-	rows, err := db.Query(query, locationTypeId)
+	names, err := queries.GetTaskTypesForLocationTypeId(ctx, int64(locationTypeId))
 	if err != nil {
-		return taskTypes, err
+		return names, fmt.Errorf("issue getting task names for a location by a task type id: %w", err)
 	}
 
-	for rows.Next() {
-		var taskTypeName string
-		err := rows.Scan(&taskTypeName)
-		if err != nil {
-			return taskTypes, err
-		}
-		taskTypes = append(taskTypes, taskTypeName)
-	}
-
-	return taskTypes, nil
+	return names, nil
 }
 
-func GetLocationFromLocationId(id int) (Location, error) {
+// GetLocationFromLocationId gets locations made by user from a locationId
+func GetLocationFromLocationId(id int) (sqlc.GetLocationByIdRow, error) {
 
 	queries := db.GetQueries()
 	ctx := context.Background()
 
-	query := `SELECT 
-		l.id, 
-		ul.id, 
-		location_type_id, 
-		latitude, 
-		longitude, 
-		ul.name, 
-		description, 
-		art 
-	FROM locations l
-	JOIN users_locations ul ON ul.location_id = l.id
-	WHERE id = $1`
-
-	row := db.QueryRow(query, id)
-
-	err := row.Scan(&location.Id, &location.LocationTypeId, &location.Latitude, &location.Longitude, &location.Name, &location.Description, &location.ArtFileName)
+	userLoc, err := queries.GetLocationById(ctx, int64(id))
 	if err != nil {
-		return location, fmt.Errorf("error scanning location data from locId: %w,", err)
+		return userLoc, fmt.Errorf("error scanning location data from locId: %w,", err)
 	}
 
-	return location, nil
+	return userLoc, nil
 }
