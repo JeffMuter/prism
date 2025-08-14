dc8b5b0b0f55ca46e97b70db72348c32e2d3834a
JeffMuter <muterjeffery@gmail.com>
2025-08-14 20:27:47 +0000

    removed profiling.

--- main.go
+++ main.go
@@ -3,30 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"prism/db"
 	"prism/logic"
 	"prism/menus"
 	"prism/render"
 	"prism/user"
-	"runtime"
-	"runtime/pprof"
 	"time"
 )
 
 func main() {
-	// CPU profiling
-	cpuFile, err := os.Create("cpu.prof")
-	if err != nil {
-		panic(err)
-	}
-	defer cpuFile.Close()
 
-	pprof.StartCPUProfile(cpuFile)
-	defer pprof.StopCPUProfile()
-
-	err = db.OpenDatabase()
-
+	err := db.OpenDatabase()
 	if err != nil {
 		log.Fatalf("db connections failed...: %v", err)
 	}
@@ -41,42 +28,26 @@ func main() {
 	}
 
 	thisUser.Latitude, thisUser.Longitude, err = user.Ping()
+
 	fmt.Printf("Lat: %v\nLong: %v\n", thisUser.Latitude, thisUser.Longitude)
 
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	err = logic.UpdateAllLocationsResourcesQuantities(thisUser.Id)
-
 	if err != nil {
 		err = fmt.Errorf("error updating all user's locations quantities: %v", err)
 		fmt.Println(err)
 	}
 
 	_, err = render.PaintScreen(&thisUser)
-
 	if err != nil {
 		err = fmt.Errorf("error from painting screen: %w\n", err)
 		fmt.Println(err)
 	}
 
 	err = menus.MainMenuListen(thisUser)
-
 	if err != nil {
 		fmt.Println(fmt.Errorf("problem in main menu: %v", err))
 	} else {
 
 		fmt.Println("Main menu closed...")
 	}
-
-	// Memory profiling
-	memFile, err := os.Create("mem.prof")
-	if err != nil {
-		panic(err)
-	}
-	defer memFile.Close()
-
-	runtime.GC() // Force GC before memory profile
-	pprof.WriteHeapProfile(memFile)
 }
